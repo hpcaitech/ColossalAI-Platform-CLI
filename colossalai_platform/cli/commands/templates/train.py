6349300dea5fc81e7de1c51c169a9d22f8a23882
FrankLeeeee <somerlee.9@gmail.com>
2023-07-11 15:51:14 +0800

    [project] init cli

---
+++ colossalai_platform/cli/commands/templates/train.py
@@ -0,0 +1,28 @@
+import os
+
+# ==================================
+# ColossalAI Platform Specification
+# DO NOT MODIFY!
+# Start
+# ==================================
+
+PROJECT_DIR = os.environ['PROJECT_DIR']
+DATASET_DIR = os.environ['DATASET_DIR']
+OUTPUT_DIR = os.environ['OUTPUT_DIR']
+CHECKPOINT_DIR = os.path.join(OUTPUT_DIR, 'checkpoint')
+TENSORBOARD_DIR = os.path.join(OUTPUT_DIR, 'tensorboard')
+
+def create_if_not_exist(path):
+    # create if not exists
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+
+create_if_not_exist(CHECKPOINT_DIR)
+create_if_not_exist(TENSORBOARD_DIR)
+
+
+# ==================================
+# ColossalAI Platform Specification
+# DO NOT MODIFY!
+# End
---
+++ setup.py
@@ -0,0 +1,68 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def fetch_requirements(path):
+    with open(path, 'r') as fd:
+        return [r.strip() for r in fd.readlines()]
+
+
+def fetch_readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+def get_version():
+    with open('version.txt') as f:
+        return f.read().strip()
+
+
+def get_template_files():
+    # get current file dir directory
+    # the templates files are located in current_file_path/templates
+    current_file_path = os.path.dirname(os.path.abspath(__file__))
+    template_file_dir = os.path.join(current_file_path, 'colossalai_platform/cli/commands/templates')
+    
+    # get all the files in template_file_dir
+    template_files = []
+    
+    for file_name in os.listdir(template_file_dir):
+        template_files.append(os.path.join(template_file_dir, file_name))
+    return template_files
+
+
+setup(
+    name='colossalai_platform',
+    version=get_version(),
+    packages=find_packages(exclude=(
+        'build',
+        'docker',
+        'tests',
+        'docs',
+        'examples',
+        '*.egg-info',
+    )),
+    description='ColossalAI Platform CLI for ease',
+    long_description=fetch_readme(),
+    long_description_content_type='text/markdown',
+    license='Apache Software License 2.0',
+    url='https://github.com/hpcaitech/ColossalAI-Platform-CLI',
+    project_urls={
+        'Github': 'https://github.com/hpcaitech/ColossalAI-Platform-CLI',
+    },
+    package_data={'': get_template_files()},
+    include_package_data=True,
+    install_requires=fetch_requirements('requirements.txt'),
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        cap=colossalai_platform.cli:cli
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Cloud :: Command Line Tool',
+        'Topic :: System :: Distributed Computing',
+    ],
+)
