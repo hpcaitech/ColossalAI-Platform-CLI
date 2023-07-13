665ba9660991153512a46ecf1d29f11bd2ee6b5b
FrankLeeeee <somerlee.9@gmail.com>
2023-07-13 14:08:20 +0800

    added tensorboard hack for volcengine

--- colossalai_platform/cli/commands/templates/train.py
+++ colossalai_platform/cli/commands/templates/train.py
@@ -1,11 +1,17 @@
 import os
 
+from tensorboard.compat.tensorflow_stub.io.gfile import LocalFileSystem
+
 # ==================================
 # ColossalAI Platform Specification
 # DO NOT MODIFY!
 # Start
 # ==================================
 
+# hack to make tensorboard compatible with Cloud File System
+delattr(LocalFileSystem, "append")
+
+# Environment variables for your data access
 PROJECT_DIR = os.environ['PROJECT_DIR']
 DATASET_DIR = os.environ['DATASET_DIR']
 OUTPUT_DIR = os.environ['OUTPUT_DIR']
@@ -13,6 +19,7 @@ CHECKPOINT_DIR = os.path.join(OUTPUT_DIR, 'checkpoint')
 TENSORBOARD_DIR = os.path.join(OUTPUT_DIR, 'tensorboard')
 
 
+# create directories
 def create_if_not_exist(path):
     # create if not exists
     if not os.path.exists(path):
