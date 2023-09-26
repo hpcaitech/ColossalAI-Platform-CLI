aa20bfca6f1694c17fc4cd9a618c23bd186c12fd
Ofey Chan <ofey206@gmail.com>
2023-09-26 11:07:44 +0800

    fix: Updated API server configuration for production

--- colossalai_platform/cli/config.py
+++ colossalai_platform/cli/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, HttpUrl
 
 
 class Config(BaseModel):
-    api_server: str = "https://luchentech.com"
+    api_server: str = "https://101.126.46.176"
     username: str = ""
     password: str = ""
     max_upload_chunk_bytes: int = 1024 * 1024 * 1024    # 1 GB
